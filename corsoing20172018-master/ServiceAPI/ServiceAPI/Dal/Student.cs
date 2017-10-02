e48cf11474b194ef56d74d44ee21c3f6a7c7e419
LuisP90 <previtiluis@gmail.com>
2017-10-02 10:41:40 +0200

    Add files via upload

---
+++ corsoing20172018-master/ServiceAPI/ServiceAPI/Dal/Student.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceAPI.Dal
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
